660d605ee2ae2bb61853bba4768ccb679b8bc15a
Shinrael <mircoelena89@gmail.com>
2024-05-31 17:44:17 +0200

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+import axios from 'axios';
+import {store} from './data/store'
+
+  export default {
+    data(){
+      return {
+        projects: [],
+      }
+    },
+
+    methods:{
+      getApi(){
+        axios.get(store.apiUrl)
+          .then(result => {
+            console.log(result.data);
+            this.projects = result.data
+        })
+        .catch(error => {
+          console.log(error.message);
+        })
+      }
+    },
+
+    mounted(){
+      this.getApi()
+    },    
+}
+</script>
+
+<template>
+  <div>
+    <h2>Lista Progetti</h2>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.id }} - {{ project.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>

661d02faacc46e24d510f31fce7c441ccdefd9d9
Shinrael <mircoelena89@gmail.com>
2024-06-03 17:09:16 +0200

    [add] router-link

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,29 @@
 <script>
 import axios from 'axios';
 import {store} from './data/store'
+import Header from './components/Header.vue'
 
   export default {
+    components:{
+      Header
+    },
     data(){
       return {
-        projects: [],
       }
     },
 
     methods:{
-      getApi(){
-        axios.get(store.apiUrl)
-          .then(result => {
-            console.log(result.data);
-            this.projects = result.data
-        })
-        .catch(error => {
-          console.log(error.message);
-        })
-      }
     },
 
     mounted(){
-      this.getApi()
     },    
 }
 </script>
 
 <template>
   <div>
-    <h2>Lista Progetti</h2>
-    <ul>
-      <li v-for="project in projects" :key="project.id">
-        {{ project.id }} - {{ project.title }}
-      </li>
-    </ul>
+    <Header />
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" :to="{ name: 'home' }">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ name: 'projects' }">Progetti</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to="{ name: 'about' }">Chi siamo</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
